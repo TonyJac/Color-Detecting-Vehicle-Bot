e4b57025a26e6b173cd7f436a662638a65428d7e
Tony Jacob <tony.jacob2018@vitstudent.ac.in>
2020-02-26 02:52:54 +0530

    arduino code

---
+++ py_serial.ino
@@ -0,0 +1,20 @@
+char serialdata;
+int pin=13;
+void setup(){
+  Serial.begin(9600);
+  pinMode(pin,OUTPUT);
+}
+ 
+void loop() {
+  while(Serial.available()>0)
+  serialdata = Serial.read();
+  Serial.print(serialdata);
+
+  if(serialdata == '1'){
+    digitalWrite(pin,HIGH);
+    }
+ 
+  else if(serialdata=='0'){
+    digitalWrite(pin,LOW);
+    }
+}
